8b73ea12d6f3f6d6203dedd4b9f69a3c743cfd1a
= <=>
2023-03-18 20:42:20 -0700

    Fix: Printing out a combination of 3 base10 digits without repetition using putchar

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -4,6 +4,7 @@
  * Printing all possible combinations
  * of bas10 digits without repetitions
  * 013 and 031 are considered repetition
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -18,11 +19,6 @@ int main(void)
 			for (num2 = 2; num2 < 10; num2++)
 			{
 				/**
-				 * Remove leading space
-				 */
-				if (num != 0 && num2 != 2)
-					putchar(' ');
-				/**
 				 * Check that current combination
 				 * isn't a duplicate
 				 */
@@ -30,14 +26,19 @@ int main(void)
 				{
 					continue;
 				}
-					putchar(48 + num);
-					putchar(48 + num1);
-					putchar(48 + num2);
-					/**
-					 * Removing trailing Comma
-					 */
-					if (num != 7 && num1 != 8 && num2 != 9)
-					putchar(',');
+				/*Check to remove leading space*/
+				if (num != 0 ||	num1 != 1 || num2 != 2)
+				{
+					putchar(' ');
+				}
+				putchar(48 + num);
+				putchar(48 + num1);
+				putchar(48 + num2);
+				/*Check to remove trailing comma*/
+				if (num != 7 || num1 != 8 || num2 != 9)
+				{
+				putchar(',');
+				}
 			}
 		}
 	}
