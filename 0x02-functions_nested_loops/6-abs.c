abb84fa8a580b03812e93c56736d448906d75cd3
= <=>
2023-03-22 09:57:32 -0700

    printing absolute values of integers

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - Returns the absolute value of an int
+ *
+ * @n: argument to be passed to _abs fxn, should be an int value
+ *
+ * Return: The absolute value of an integer
+ */
+int _abs(int n)
+{
+	return (abs(n));
+}
