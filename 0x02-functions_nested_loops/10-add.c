9a883c6da64d93a3ce1215a0f58e851ad60ef5ec
= <=>
2023-03-22 19:53:10 -0700

    summing up two int values

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * add - adds two int values
+ *
+ * @num1: first argument to be summed up
+ * @num2: second argument to be summed up
+ *
+ * Return: Sum of two int vals
+ */
+int add(int num1, int num2)
+{
+	int sum;
+
+	sum = num1 + num2;
+
+	return (sum);
+}
+
