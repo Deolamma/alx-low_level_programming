04e454a58fac545352d731d26b7eb0a491da2ef3
= <=>
2023-03-24 19:33:20 -0700

    printing int vals 0-9 skip 2 & 4 using putchar

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_most_numbers - Prints most numbers btwn 0&9
+ *
+ * Return: always returns an integer when eturn type is void
+ */
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		if (a == 2 || a== 4)
+			continue;
+		_putchar('0' + a);
+	}
+	_putchar('\n');
+}
