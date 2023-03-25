646e7336632677b7e5958f1d397cac168b663ede
= <=>
2023-03-24 17:42:34 -0700

    Checking the case status of a char

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _isupper - checks if an int(char) passed is upper
+ *
+ * @c: argument to be passed into fxn isupper
+ *
+ * Return: 1 if arg isupper, 0 otherwise
+ */
+int _isupper(int c)
+{
+	if ((isupper(c)) != 0)
+			return (1);
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,4 @@
+# MORE ON FUNCTIONS AND NESTED LOOPS
+- Creating pyrmamids using loops
+- Creating more abstractions of system fxns e.g isupper, isdigit e.t.c
+- More practice on creating and using prototypes & header files
