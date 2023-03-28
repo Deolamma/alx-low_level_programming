a597054ec43de74483dbd2842306470643f289c9
= <=>
2023-03-27 19:16:41 -0700

    printing a str., followed by new line using puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - prints a string and inserts a new line
+ * @str: string parameter to be printed out
+ *
+ * Return: Returns void (Always 0)
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
