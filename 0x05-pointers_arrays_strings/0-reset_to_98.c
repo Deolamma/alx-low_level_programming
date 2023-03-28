c6a2484dec506a6461efe1f9ecf259537314c394
= <=>
2023-03-27 17:15:29 -0700

    Setting a pointer to point to a diff value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - Updating the value a pointer points to, to 98
+ *
+ * @n: pointer to be updated
+ *
+ * Return: Returns void (Always an int)
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
