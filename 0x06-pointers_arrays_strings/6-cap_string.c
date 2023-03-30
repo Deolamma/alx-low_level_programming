8be27ec53f160989e7480b9fd15776a89aceb438
= <=>
2023-03-30 12:08:28 -0700

    betty style doc. fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,14 +12,21 @@ char *cap_string(char *str)
 
 	while (str[i] != '\0')
 	{
-		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'
-		|| str[i] == ',' || str[i] == ';' || str[i] == '.' ||
-		str[i] == '!' || str[i] == '?' || str[i] == '"' ||
-		str[i] == '(' || str[i] == ')' || str[i] == '{' ||
-		str[i] == '}')
+		if (str[i] == ' ' ||
+		    str[i] == '\t' ||
+		    str[i] == '\n' ||
+		    str[i] == ',' ||
+		    str[i] == ';' ||
+		    str[i] == '.' ||
+		    str[i] == '!' ||
+		    str[i] == '?' ||
+		    str[i] == '"' ||
+		    str[i] == '(' ||
+		    str[i] == ')' ||
+		    str[i] == '{' ||
+		    str[i] == '}')
 		{
-			if (str[i + 1] != '\0' && str[i + 1] >= 'a'
-			&& str[i + 1] <= 'z')
+			if (str[i + 1] != '\0' && str[i + 1] >= 'a' && str[i + 1] <= 'z')
 			{
 				str[i + 1] = str[i + 1] - 32;
 			}
