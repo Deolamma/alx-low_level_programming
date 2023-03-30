b4419e362b9914dbf5f3fdf4729ea65c129435db
= <=>
2023-03-30 08:23:38 -0700

    Changing lowercase letters to uppercase letters in a str. array

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * string_toupper - converts strings from lower to uppercase
+ *
+ * @str: str array that'll be passed into the fxn
+ *
+ * Return: Returns a array pf strings
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] != ' ' && str[i] != '\n')
+		{
+			if (str[i] > 65 && str[i] <= 90)
+			{
+				str[i] = str[i] - 0;
+			} else
+			{
+				str[i] = str[i] - 32;
+			}
+		}
+		i++;
+	}
+	return (str);
+}
+
