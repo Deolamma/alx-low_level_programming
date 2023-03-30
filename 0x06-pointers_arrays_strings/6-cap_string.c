643adebddd5956ff26e338256c71fc83b82734d3
= <=>
2023-03-30 12:13:34 -0700

    betty style doc. fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -26,7 +26,9 @@ char *cap_string(char *str)
 		    str[i] == '{' ||
 		    str[i] == '}')
 		{
-			if (str[i + 1] != '\0' && str[i + 1] >= 'a' && str[i + 1] <= 'z')
+			if (str[i + 1] != '\0' &&
+			    str[i + 1] >= 'a' &&
+			    str[i + 1] <= 'z')
 			{
 				str[i + 1] = str[i + 1] - 32;
 			}
