d5f86b5438d5bf7b8ca8a80a9246cd71ecc792a1
= <=>
2023-03-30 10:49:14 -0700

    capitalizing first letters of words in a string array

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * cap_string - Capitalize first words after spaces and special characters
+ *
+ * @str: string array that'll be passed into the fxn
+ *
+ * Return: Returns a string array with capitalized first words
+ */
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'
+		|| str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+		str[i] == '!' || str[i] == '?' || str[i] == '"' ||
+		str[i] == '(' || str[i] == ')' || str[i] == '{' ||
+		str[i] == '}')
+		{
+			if (str[i + 1] != '\0' && str[i + 1] >= 'a'
+			&& str[i + 1] <= 'z')
+			{
+				str[i + 1] = str[i + 1] - 32;
+			}
+		}
+		i++;
+	}
+	return (str);
+}
