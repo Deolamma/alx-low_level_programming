f85d4ef167a444b73aa6d0b5c97e0128113e8f81
= <=>
2023-04-03 10:22:38 -0700

    creating an abstraction of strspn fxn

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,6 +3,7 @@
 
 char *_memset(char *, char, unsigned int);
 char *_memcpy(char *, char *, unsigned int);
-char *_strchr( char *, char);
+char *_strchr(char *, char);
+unsigned int _strspn(char *, char *);
 
 #endif
