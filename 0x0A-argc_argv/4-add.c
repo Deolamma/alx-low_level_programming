589a3ec97ccd91fcabb0a2eebd13145580de8568
= <=>
2023-04-09 13:14:19 -0700

    Summing up int arguments passed to fxn main

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+/**
+ * main - prints out the sum of all arguments passed unto main
+ *
+ * @argc: stores the count of int passed into main fxn
+ * @argv: stores the arguments passed into main fxn
+ *
+ * Return: returns 0 (Success) or 1 (failure)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum = 0;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			/**
+			 * *argv[i] gives us the actual value
+			 * of data stored in argv, hence enabling
+			 * our ability to ascertain numerical nature
+			 * of data stored
+			 */
+			if (isdigit(*argv[i]))
+			{
+				sum += atoi(argv[i]);
+			} else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", sum);
+	}
+	return (0);
+}
+
