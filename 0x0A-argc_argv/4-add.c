12a52e11ddb065e7a33ab8ad64d3cbc8b851feea
= <=>
2023-04-10 09:00:55 -0700

    Code fix to return 0 if no int is passed as arg

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <stdlib.h>
+#include "main.h"
 /**
  * main - prints out the sum of all arguments passed unto main
  *
@@ -13,19 +14,31 @@ int main(int argc, char *argv[])
 {
 	int i;
 	int sum = 0;
+	int count = 0;
 
 	if (argc == 1)
 		printf("0\n");
 	if (argc > 2)
 	{
-
+		/**
+		 * checks that no int digit was passed as an argument and prints out 0
+		 */
+		for (i = 1; i < argc; i++)
+		{
+			if (isdigit(*argv[i]))
+				count++;
+		}
+		if (count < argc - 1)
+		{
+			printf("0\n");
+			return (1);
+		}
 		for (i = 1; i < argc; i++)
 		{
 			/**
 			 * *argv[i] gives us the actual value
 			 * of data stored in argv, hence enabling
-			 * our ability to ascertain numerical nature
-			 * of data stored
+			 * our ability to ascertain numerical nature of data stored
 			 */
 			if (isdigit(*argv[i]))
 			{
@@ -40,4 +53,3 @@ int main(int argc, char *argv[])
 	}
 	return (0);
 }
-
