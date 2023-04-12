acfeab70de0ca1c581bbcc6a6d8f2ea9783983a5
= <=>
2023-04-11 18:41:57 -0700

    Fix: NULL char arr should be treated as empty str

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,11 +18,10 @@ char *str_concat(char *s1, char *s2)
 	int ss2 = 0;
 	char *myarr;
 
-	if (s1 == NULL || s2 == NULL)
-	{
+	if (s1 == NULL)
 		s1 = "";
+	if (s2 == NULL)
 		s2 = "";
-	}
 	for (a = 0; s1[a] != '\0'; a++)
 	{
 		ss1++;
