1fda11b73d44e4a3005383f78d674f42d0b05038
= <=>
2023-04-17 17:39:20 -0700

    Creating a dog struct type

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - defines few properties of dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: Nothing more to describe
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
