f2577eceb26651454fd70b4a6e37820424e9fc16
= <=>
2023-04-17 20:16:28 -0700

    printing elements of struct dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,15 +13,15 @@ void print_dog(struct dog *d)
 	{
 		printf("Name: (nil)\n");
 	} else
-		printf("Name: %s\n", d->name);
-	if (d->age < 0.0)
 	{
-		printf("age: (nil)\n");
-	} else
-		printf("age: %.6f\n", d->age);
+		printf("Name: %s\n", d->name);
+	}
+	printf("age: %.6f\n", d->age);
 	if (d->owner == NULL)
 	{
 		printf("Owner: (nil)\n");
 	} else
+	{
 		printf("Owner: %s\n", d->owner);
+	}
 }
