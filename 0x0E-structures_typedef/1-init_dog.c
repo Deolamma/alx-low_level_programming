97a9bb1ec879824f86bd5ef89a6c7ae292672c37
= <=>
2023-04-17 18:42:25 -0700

    initializing dog struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,36 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * init_dog - this initializes dog struct type
+ * @d: this is a new instance of dog;
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Return: Nothing
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	int i;
+	int namelen = 0;
+	int ownlen = 0;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+	for (i = 0; name[i] != '\0'; i++)
+		namelen++;
+	d->name = malloc(sizeof(char) * (namelen + 1));
+	for (i = 0; name[i] != '\0'; i++)
+	{
+		d->name[i] = name[i];
+	}
+	d->name[i] = '\0';
+	d->age = age;
+	for (i = 0; owner[i] != '\0'; i++)
+		ownlen++;
+	d->owner = malloc(sizeof(char) * (ownlen + 1));
+	for (i = 0; owner[i] != '\0'; i++)
+	{
+		d->owner[i] = owner[i];
+	}
+	d->owner[i] = '\0';
+}
