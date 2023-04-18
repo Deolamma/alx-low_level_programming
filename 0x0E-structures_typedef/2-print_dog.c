a4ab05a6b40006b8b89fcfbcb29e5d56b729fdaf
= <=>
2023-04-17 19:40:35 -0700

    printing struct dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,27 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog - prints the property of a dog
+ * @d: dog struct
+ * Return: Nothing
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		printf("nil\n");
+	if (d->name == NULL)
+	{
+		printf("Name: (nil)\n");
+	} else
+		printf("Name: %s\n", d->name);
+	if (d->age < 0.0)
+	{
+		printf("age: (nil)\n");
+	} else
+		printf("age: %.6f\n", d->age);
+	if (d->owner == NULL)
+	{
+		printf("Owner: (nil)\n");
+	} else
+		printf("Owner: %s\n", d->owner);
+}
