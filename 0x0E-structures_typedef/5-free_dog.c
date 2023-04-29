671a039468579b982cbb9a44975afcd06e9de550
Deolamma <adebanjoemmanuel01@gmail.com>
2023-04-29 03:52:54 -0700

    Fix: handling null case of dog_t

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <stddef.h>
 /**
  * free_dog - free up mem. allocated to dog_t struct
  * @d: free up space allocated to the struct
@@ -8,6 +9,10 @@
  */
 void free_dog(dog_t *d)
 {
+	if (d == NULL)
+	{
+		return;
+	}
 		free(d->name);
 		free(d->owner);
 		free(d);
