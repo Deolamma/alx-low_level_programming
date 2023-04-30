db56e30a69f43846a087a0d8baa7aed2a13c8d5a
Deolamma <adebanjoemmanuel01@gmail.com>
2023-04-29 17:38:00 -0700

    putchar fxn abstraction

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _putchar - prints one char at a time
+ * @c: char to be printed out
+ * Return: returns an int
+ */
+int _putchar(int c)
+{
+	putchar(c);
+	return (0);
+}
