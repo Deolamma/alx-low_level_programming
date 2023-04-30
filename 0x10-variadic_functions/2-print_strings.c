12fbc3045541a0d5d9d1994ad852e8e51e2a783a
Deolamma <adebanjoemmanuel01@gmail.com>
2023-04-30 06:32:09 -0700

    implementing realloc fxn using my own implementation of memcpy

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -2,27 +2,35 @@
 #include <stdarg.h>
 #include <stdio.h>
 /**
- * print_numbers - prints unknown variables separated by separator
+ * print_strings - prints unknown variables separated by separator
  * @separator: character to be placed after each variable
  * @n: number of variables to be passed into function
  * @...: Unknown variables to be worked upon
  * Return: Nothing
  */
-void print_numbers(const char *separator, const unsigned int n, ...)
+void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list myargs;
 	int i = 0;
-	int x;
+	char x;
 
+	if (n == 0)
+		return;
 	va_start(myargs, n);
 	while (i < (int) n)
 	{
-		x = va_arg(myargs, int);
-		separator != NULL ? printf("%d%s", x,
-		i == (int) n - 1 ? "" : separator) : printf("%d", x);
+		x = va_arg(myargs, char);
+		if (separator == NULL)
+		{
+			printf("%s%s", x, separator);
+		} else
+		{
+			printf("(nil)"%s	
+
+
+
 		i++;
 	}
 	printf("\n");
-	va_end(myargs);
 }
 
