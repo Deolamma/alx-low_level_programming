28f8b6cc3ebec980bea9c4b89b31b7bc7ea3cfc7
Deolamma <adebanjoemmanuel01@gmail.com>
2023-05-01 11:41:00 -0700

    freeing up memory allocated to SLL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * free_listint2 - frees memory allcated to SLL
+ * @head: pointer to the pointer to the first element of SLL
+ * Return: Nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *curNode;
+	listint_t *tmp;
+
+	curNode = *head;
+
+	while (curNode != NULL)
+	{
+		tmp = curNode->next;
+		free(curNode);
+		curNode = tmp;
+	}
+	*head = NULL;
+}
