1a891ecf4ea94511212cbab32e8ac619b84d731f
Deolamma <adebanjoemmanuel01@gmail.com>
2023-05-02 18:57:13 -0700

    printing a node based on index passed

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * get_nodeint_at_index - gets the node at a certain index
+ * @head: pointer to address of first element
+ * @index: index of node to be returned
+ * Return: node at index
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int strt_index = 0;
+
+	if (head == NULL)
+		return (NULL);
+	if (index == 0)
+		return (head);
+	while (head->next != NULL)
+	{
+		if (strt_index == index)
+			return (head);
+		strt_index++;
+		head = head->next;
+	}
+	return (NULL);
+}
+
