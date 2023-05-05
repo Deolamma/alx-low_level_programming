8cd60d706f3262d00a6b19a3164d01a0573e9501
Deolamma <adebanjoemmanuel01@gmail.com>
2023-05-04 18:20:52 -0700

    set bit at certain index to 0

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,5 +5,6 @@ int _putchar(int);
 void print_binary(unsigned long int);
 int get_bit(unsigned long int, unsigned int);
 int set_bit(unsigned long int *, unsigned int);
+int clear_bit(unsigned long int *, unsigned int);
 
 #endif /* end #ifndef MAIN_H */
