6e8ce1287d8bf009140808cc3fda839f7b56eda4
Deolamma <adebanjoemmanuel01@gmail.com>
2023-05-11 20:50:47 -0700

    Creating a new file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,7 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdlib.h>
+#define PERMS 0600
 
 ssize_t read_textfile(const char *, size_t);
+int create_file(const char *, char *);
 
 #endif /* end #ifndef MAIN_H */
