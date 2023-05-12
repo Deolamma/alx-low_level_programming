75f135cad274a43031ac88dd56e443c37fc299ae
Deolamma <adebanjoemmanuel01@gmail.com>
2023-05-12 08:50:33 -0700

    Fix: Not assigning new permission to an existing file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,14 +16,16 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, PERMS);
+	fd = access(filename, F_OK) == 0 ?
+		open(filename, O_WRONLY | O_TRUNC) :
+		open(filename, O_WRONLY | O_CREAT, PERMS);
 	if (fd == -1)
 		return (-1);
-	if (text_content != NULL)
+	if (text_content != NULL || access(filename, W_OK) == 0)
 	{
 		for (i = 0; text_content[i] != '\0'; i++)
 			count++;
-		txt_content_len = (ssize_t) count + 1;
+		txt_content_len = (ssize_t) count;
 		written_bytes = write(fd, text_content, txt_content_len);
 		if (written_bytes == -1 || written_bytes != txt_content_len)
 		{
