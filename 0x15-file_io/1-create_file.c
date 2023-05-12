1c4164a46f07f40f034366359156b77a94173308
Deolamma <adebanjoemmanuel01@gmail.com>
2023-05-11 21:25:26 -0700

    Fix: if check for failur in writing to created file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 			count++;
 		txt_content_len = (ssize_t) count + 1;
 		written_bytes = write(fd, text_content, txt_content_len);
-		if (written_bytes != txt_content_len)
+		if (written_bytes == -1 || written_bytes != txt_content_len)
 		{
 			close(fd);
 			return (-1);
