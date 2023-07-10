e147909b077a07f207dc9be9f91b6efb2878d0e1
Deolamma <adebanjoemmanuel01@gmail.com>
2023-07-10 14:25:55 -0700

    Fix: Error handling and closing of file descriptors

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,16 +15,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	/* number of bytes read */
 	ssize_t nb_read;
 	ssize_t nb_written;
-	char buf[BUFFSIZ];
+	char buf[letters];
 
 	if (filename == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-	{
-		close(fd);
 		return (0);
-	}
 	nb_read = read(fd, buf, letters);
 	if (nb_read == -1)
 	{
@@ -32,7 +29,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 	nb_written = write(STDOUT_FILENO, buf, nb_read);
-	if ((nb_read != nb_written) || (nb_written == -1))
+	if (nb_written == -1 || nb_written != nb_read)
 	{
 		close(fd);
 		return (0);
