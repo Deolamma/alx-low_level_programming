16d6707354fb0004628d66281d7dab55ad9bfb9f
Deolamma <adebanjoemmanuel01@gmail.com>
2023-07-10 15:05:28 -0700

    Fix: Proper allocation of memory

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <stddef.h>
 #include <sys/types.h>
+#include <stdlib.h>
 /**
  * read_textfile - reads text from a file and prints to stdout
  * @filename: name of file of which text is to be printed from
@@ -15,25 +16,29 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	/* number of bytes read */
 	ssize_t nb_read;
 	ssize_t nb_written;
-	char buf[letters];
+	char *buf;
 
 	if (filename == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
+	buf = malloc(sizeof(char) * letters);
 	nb_read = read(fd, buf, letters);
 	if (nb_read == -1)
 	{
+		free(buf);
 		close(fd);
 		return (0);
 	}
 	nb_written = write(STDOUT_FILENO, buf, nb_read);
 	if (nb_written == -1 || nb_written != nb_read)
 	{
+		free(buf);
 		close(fd);
 		return (0);
 	}
+	free(buf);
 	close(fd);
 	return (nb_read);
 }
