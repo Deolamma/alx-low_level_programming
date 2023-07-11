6510cb05f74d4b16a2d7e00cb40e81562f949da8
Deolamma <adebanjoemmanuel01@gmail.com>
2023-07-11 03:28:57 -0700

    Fix: Deleting separate main.c file... resulting in 1 prog. file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -24,7 +24,7 @@ void file_from_err_handler(char *file_name)
 {
 	if (file_name != NULL)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_name);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_name);
 		exit(98);
 	}
 }
@@ -38,6 +38,23 @@ void fd_err_handler(int fd_value)
 	exit(100);
 }
 /**
+ * main - passes the arguments from command line to cp fxn
+ * @argc: count of arguments passed to the CLI
+ * @argv: array of the elements passed to CLI
+ * Return: 0 on Success
+ */
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to:\n");
+		exit(97);
+	}
+	cp(argv[1], argv[2]);
+	return (0);
+}
+
+/**
  * cp - copy content from a file to another
  * @file_from: file to read content from
  * @file_to: file to write content to
@@ -47,7 +64,7 @@ void cp(char *file_from, char *file_to)
 	/* file descriptors for file_from and file_to */
 	int fd_from, fd_to;
 	char buff[BUFFSIZ];
-	ssize_t nb_read = -1, nb_written;
+	ssize_t nb_read, nb_written;
 
 	if (file_to == NULL || file_from == NULL)
 		exit(98);
@@ -57,11 +74,11 @@ void cp(char *file_from, char *file_to)
 	fd_from = open(file_from, O_RDONLY);
 	if (fd_from == -1)
 		file_from_err_handler(file_from);
-	while (nb_read > 0)
+	while ((nb_read = read(fd_from, buff, BUFFSIZ)) > 0)
 	{
-		nb_read = read(fd_from, buff, BUFFSIZ);
-		if (nb_read == -1 || nb_read != BUFFSIZ)
+		if (nb_read == -1)
 		{
+			file_from_err_handler(file_from);
 			close(fd_from);
 			if (close(fd_to) == -1)
 				fd_err_handler(fd_to);
@@ -69,6 +86,7 @@ void cp(char *file_from, char *file_to)
 		nb_written = write(fd_to, buff, nb_read);
 		if (nb_written == -1 || nb_written != nb_read)
 		{
+			file_to_err_handler(file_to);
 			close(fd_to);
 			if (close(fd_from) == -1)
 				fd_err_handler(fd_from);
