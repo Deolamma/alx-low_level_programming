b7382108c4f5f57267aadfbb5e43ae7549f1a3df
Deolamma <adebanjoemmanuel01@gmail.com>
2023-07-14 15:11:44 +0100

    Fix: spelling error

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -47,7 +47,7 @@ int main(int argc, char **argv)
 {
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to:\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	cp(argv[1], argv[2]);
