6bb5bca1824a6936672cda4551e3e3b68de242e9
= <=>
2023-08-19 07:58:44 +0100

    printing out elemnts of a DLL

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_dlistint - prints the elements of a DLL
+ * @h: points to the list to be traversed
+ * Return: Returns the number of elements in the DLL
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t counter = 0;
+
+	if (h->next == NULL)
+	{
+		printf("%d\n", h->n);
+		counter = 1;
+		return ((size_t) counter);
+	}
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		counter += 1;
+		h = h->next;
+	}
+	return (counter);
+}
