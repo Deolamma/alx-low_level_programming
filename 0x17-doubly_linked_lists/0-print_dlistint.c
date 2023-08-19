57784585b873c719e65d4f6e54e1d46f1ee70ad1
= <=>
2023-08-19 08:10:03 +0100

    Fix: Code restructure

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -14,14 +14,15 @@ size_t print_dlistint(const dlistint_t *h)
 		return (0);
 	}
 
+	if (h->next == NULL)
+	{
+		printf("%d\n", h->n);
+		counter = 1;
+		return (counter);
+	}
+
 	while (h)
 	{
-		if (h->next == NULL)
-		{
-			printf("%d\n", h->n);
-			counter = 1;
-			return (counter);
-		}
 		printf("%d\n", h->n);
 		counter += 1;
 		h = h->next;
