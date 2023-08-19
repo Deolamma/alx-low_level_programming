6c81d3451f466a3473bceb43244ff2369a7424b0
= <=>
2023-08-19 08:05:33 +0100

    Fix: code restructure and handling of edge case

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,15 +9,19 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t counter = 0;
 
-	if (h->next == NULL)
+	if (h == NULL)
 	{
-		printf("%d\n", h->n);
-		counter = 1;
-		return ((size_t) counter);
+		return (0);
 	}
 
 	while (h)
 	{
+		if (h->next == NULL)
+		{
+			printf("%d\n", h->n);
+			counter = 1;
+			return (counter);
+		}
 		printf("%d\n", h->n);
 		counter += 1;
 		h = h->next;
