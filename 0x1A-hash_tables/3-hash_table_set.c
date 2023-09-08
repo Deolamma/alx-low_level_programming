3dbb63823791ce6ce720e33d911c961ede1f55ed
Deolamma <=>
2023-09-08 15:05:44 +0100

    Creating a new key-value pair and updating the value of an existing key

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,51 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - we set or update the key/value of hash table
+ * @ht: This is the hash table we want to add or update its key/value
+ * @key: This is the key which will be mapped to a value
+ * @value: This is the value associated with a key
+ * Return: returns 1 on Success or 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new_node, *existing_node;
+	unsigned long int index;
+
+	if (!ht || !key || *key != '\0' || !value || ht->size == 0)
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	existing_node = ht->array[index];
+	while (existing_node)
+	{
+		if (strcmp(existing_node->key, key) == 0)
+		{
+			free(existing_node->value);
+			existing_node->value = strdup(value);
+			if (!existing_node->value)
+				return (0);
+			return (1);
+		}
+		existing_node = existing_node->next;
+	}
+
+	/* allocate memory for the new_node or entry into the hash table */
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
+		return (0);
+	new_node->key = strdup(key);
+	if (!new_node->key)
+	{
+		free(new_node);
+		return (0);
+	}
+	new_node->value = strdup(value);
+	if (!new_node->value)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (0);
+	}
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
+}
