58f647628fce14024778d6f1e4a6c92526f97cae
Deolamma <=>
2023-09-08 15:14:53 +0100

    Fix: Handling of case when key is empty string

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,7 +11,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node, *existing_node;
 	unsigned long int index;
 
-	if (!ht || !key || *key != '\0' || !value || ht->size == 0)
+	if (!ht || !key || *key == " " || !value || ht->size == 0)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	existing_node = ht->array[index];
