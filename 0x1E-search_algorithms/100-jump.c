9c19e59a12263a03c9555fb22ca5689b507bf2cf
Deolamma <adebanjoemmanuel01@gmail.com>
2023-12-17 00:31:23 +0100

    Implementing jump search algorithm

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,42 @@
+#include "search_algos.h"
+
+/**
+ * jump_search - implements the jump searching algorithm
+ * @array: Array to be searched
+ * @size: size of the array to be searched
+ * @value: value to search for in array
+ * Return: returns the value to be searched or -1
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	int prev = 0;
+	int step, i;
+
+	if (!array || size == 0)
+		return (-1);
+
+	step = (int) sqrt(size);
+
+	while (array[step] < value && step <= (int) size)
+	{
+		printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
+		prev = step;
+		step += (int) sqrt(size);
+	}
+
+	/* COMMENT OUT TO UNFERSTAND WHAT GOES WRONG */
+	if (array[step] >= value || step > (int) size)
+		printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
+
+
+	printf("Value found between indexes [%d] and [%d]\n", prev, step);
+
+	for (i = prev; (i <= step) && (i < (int) size); i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (array[i]);
+	}
+
+	return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,9 +1,11 @@
 #ifndef SEARCH_ALGOS_H
 #define SEARCH_ALGOS_H
 #include <stdio.h>
+#include <math.h>
 
 /* Function declarations */
 int linear_search(int *, size_t, int);
 int binary_search(int *, size_t, int);
+int jump_search(int *, size_t, int);
 
 #endif /* end #ifndef SEARCH_ALGOS_H */
